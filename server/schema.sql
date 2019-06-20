943078de7a1a33074bd09b6631def65a31ca196c
kbsjsn <shin.kirk@gmail.com>
2019-06-19 17:44:57 -0700

    first edit on schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,35 @@
 CREATE DATABASE chat;
 
+
 USE chat;
 
+DROP TABLE IF EXISTS messages;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  message TEXT,
+  room_id INT NOT NULL,
+  PRIMARY KEY (id)  
 );
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INT NOT NULL auto_increment,
+  username text not null,
+  PRIMARY KEY (id)
+);
+drop table if exists rooms;
+
+create table rooms (
+  id int not null auto_increment,
+  room_name text not null,
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE messages ADD FOREIGN KEY (id) REFERENCES users (id);
+ALTER TABLE messages ADD FOREIGN KEY (id) REFERENCES rooms (id);
 
 /* Create other tables and define schemas for them here! */
 
