f553f90c18c12840f737d25eec3b35924ff930d5
kbsjsn <shin.kirk@gmail.com>
2019-06-19 19:29:20 -0700

    connected database and server

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,24 @@
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'student',
+    password: 'student',
+    database: 'chat'
+})
+
+connection.connect((err) => {
+    if (err) return console.log(err);
+    console.log('Database connected');
+})
+
+// connection.query('Query success??????', (err, rows, fields) => {
+//     if (err) throw err
+
+//     console.log('this is row', row[0].solution)
+// })
+
+connection.end()
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
