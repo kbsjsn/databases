343d91dca11f638ac5ae2d0aaa89e605df3cb334
kbsjsn <shin.kirk@gmail.com>
2019-06-19 20:15:14 -0700

    working on models

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,15 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {  // a function which produces all the messages
+      db.query("SELECT * FROM MESSAGES", (err, message) => {
+        if (err) throw err;
+        callback(message);
+      });
+    }, 
+    post: function () {
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
